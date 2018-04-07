58495f853c5c209f8bcc5153272db73a54dbad6f
Neil <neil.ronald.irvine@gmail.com>
2018-04-07 18:20:56 -0400

    Add winning or losing functionality.

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,7 +1,18 @@
 import random
 
 def rollDice():
-    roll = random.randint(1, 6)
-    return roll
+    roll = random.randint(1, 100)
+
+    if roll == 100:
+        print("Roll was 100, you lose.", sep="")
+        return False
+    elif roll <= 50:
+        print("Roll was ", roll, ". Too low, you lose.", sep="")
+        return False
+    elif 100 > roll > 50:
+        print("Roll was ", roll, ", you WIN.", sep="")
+        return True
+
+
 
 print(rollDice())
