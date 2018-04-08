4d000976f5088e74fe2c8091bf5a2d39427d9698
Neil <neil.ronald.irvine@gmail.com>
2018-04-08 17:09:01 -0400

    Add loop to play lots of games and determine when broke.

--- monte_carlo.py
+++ monte_carlo.py
@@ -4,13 +4,13 @@ def rollDice():
     roll = random.randint(1, 100)
 
     if roll == 100:
-        print("Roll was 100, you lose.", sep="")
+        # print("Roll was 100, you lose.", sep="")
         return False
     elif roll <= 50:
-        print("Roll was ", roll, ". Too low, you lose.", sep="")
+        # print("Roll was ", roll, ". Too low, you lose.", sep="")
         return False
     elif 100 > roll > 50:
-        print("Roll was ", roll, ", you WIN.", sep="")
+        # print("Roll was ", roll, ", you WIN.", sep="")
         return True
 
 def simple_bettor(funds, initial_wager, wager_count):
@@ -26,6 +26,12 @@ def simple_bettor(funds, initial_wager, wager_count):
             value -= wager
 
         current_wager += 1
-        print("Funds:", value)
+    if value < 0:
+        value = "broke"
+    print("Funds:", value)
 
-simple_bettor(100, 10, 50000)
+x = 0
+
+while x < 20:
+    simple_bettor(100, 100, 20)
+    x += 1
