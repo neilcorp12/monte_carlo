7a309511258fbf14a40010a55a52cfad7a64ae75
Neil <neil.ronald.irvine@gmail.com>
2018-04-11 06:29:43 -0400

    Generate and plot a simple bettor.

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,5 +1,7 @@
 import random
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
 def rollDice():
     roll = random.randint(1, 100)
@@ -18,21 +20,35 @@ def simple_bettor(funds, initial_wager, wager_count):
     value = funds
     wager = initial_wager
 
-    current_wager = 0
+    # Wager and value lists
+    wX = []
+    vY = []
 
-    while current_wager < wager_count:
+    current_wager = 1
+
+    while current_wager <= wager_count:
         if rollDice():
             value += wager
+            wX.append(current_wager)
+            vY.append(value)
         else:
             value -= wager
+            wX.append(current_wager)
+            vY.append(value)
 
         current_wager += 1
     if value < 0:
         value = "broke"
-    print("Funds:", value)
+    # print("Funds:", value)
 
+    plt.plot(wX, vY)
 x = 0
 
 while x < 20:
     simple_bettor(100, 100, 20)
     x += 1
+
+# Show the chart
+plt.ylabel('Account Value')
+plt.xlabel('Wage Count')
+plt.show()
