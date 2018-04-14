a00db9a676107bb47d2f3a42cf56635de19391c0
Neil <neil.ronald.irvine@gmail.com>
2018-04-14 22:13:26 +0100

    Add multiple bettor.

--- monte_carlo.py
+++ monte_carlo.py
@@ -4,6 +4,9 @@ import matplotlib
 import matplotlib.pyplot as plt
 import time
 
+lower_bust    = 31.235
+higher_profit = 63.208
+
 sample_size = 1000
 starting_funds = 10000
 wager_size = 100
@@ -18,6 +21,68 @@ def rollDice():
         return False
     elif 100 > roll > 50:
         return True
+
+def multiple_bettor(funds, initial_wager, wager_count, colour='c'):
+    global multiple_busts
+    global multiple_profits
+
+    value = funds
+    wager = initial_wager
+
+    wX = []
+    wY = []
+
+    current_wager  = 1
+    previous_wager = 'win'
+    previous_wager_amount = initial_wager
+
+    while current_wager <= wager_count:
+        if previous_wager == 'win':
+            if rollDice():
+                value += wager
+                wX.append(current_wager)
+                vY.append(value)
+            else:
+                value -= wager
+                previous_wager = 'loss'
+                previous_wager_amount = wager
+                wX.append(current_wager)
+                vY.append(value)
+
+                if value <= 0:
+                    multiple_busts += 1
+                    break
+        elif previous_wager == 'loss':
+            if rollDice():
+                wager = previous_wager_amount * random_multiple
+                if (value - wager) < 0:
+                    wager = value
+                value += wager
+                wager = initial_wager
+                previous_wager = 'win'
+                wX.append(current_wager)
+                vY.append(value)
+            else:
+                wager = previous_wager_amount * random_multiple
+                if (value - wager) < 0:
+                    wager = value
+                value -= wager
+                previous_wager_amount = wager
+                wX.append(current_wager)
+                vY.append(value)
+
+                if value <= 0:
+                    multiple_busts += 1
+                    break
+                previous_wager = 'loss'
+        current_wager += 1
+    # plt.plot(wX, vY, colour)
+    if value > funds:
+        multiple_profits += 1
+
+
+
+
 
 def doubler_bettor(funds, initial_wager, wager_count, colour='m'):
     value = funds
@@ -109,23 +174,20 @@ def simple_bettor(funds, initial_wager, wager_count, colour='k'):
         value = 0
         simple_profits += 1
 
-x = 0
-simple_profits  = 0.0
-simple_busts    = 0.0
-doubler_profits = 0.0
-doubler_busts   = 0.0
+while True:
+    multiple_profits  = 0.0
+    multiple_busts    = 0.0
+    multiple_sample_size = 1000
 
-while x < sample_size:
-    simple_bettor(starting_funds, wager_size, wager_count)
-    doubler_bettor(starting_funds, wager_size, wager_count)
-    x += 1
 
-print("Simple bettor profit ratio:", (simple_profits/sample_size)*100.00)
-print("Doubler bettor profit ratio:", (doubler_profits/sample_size)*100.00)
-print("Simple bettor bust ratio:", (simple_busts/sample_size)*100.00)
-print("Doubler bettor bust ratio:", (doubler_busts/sample_size)*100.00)
+    current_sample = 1
+    random_multiple = random.uniform(0.1, 10.0)
 
-plt.axhline(0, color='r')
-plt.ylabel('Account Value')
-plt.xlabel('Wage Count')
-plt.show()
+    while current_sample <= multiple_sample_size:
+        multiple_bettor(starting_funds, wager_size, wager_count)
+
+
+# plt.axhline(0, color='r')
+# plt.ylabel('Account Value')
+# plt.xlabel('Wage Count')
+# plt.show()
